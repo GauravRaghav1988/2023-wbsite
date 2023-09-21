600ffc055963ebf98a59c7a7e4676cd763172f17
GAURAV RAGHAV <gaurav@brancosoft.co.in>
2023-09-22 03:14:48 +0530

    added some latest features but need to check but all working fine

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,22 +1,20 @@
 from django.contrib import admin
 from django.urls import path
 from .import views
-# from .views import create_user, users_list,courses_list,create_course
-
-
-
+# from myapp.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('',views.home),
-    # path('about/',views.about),
-    # # path('records/',views.records),
     path('registration/',views.register),
     path('login/',views.user_login),
-    # path('success/',views.success),
     path('account/',views.account), 
     path('changepassword/',views.change_password),
-    path('logout/',views.logoutuser),
-
-
-    
+    path('logout/',views.logoutuser),   
+    path('quiz/',views.quiz_home,name='quiz_home'),
+    path('add_question/',views.addQuestion,name='addQuestion'),
+ 
 ]
+
+
